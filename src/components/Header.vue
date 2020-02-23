024d7ed4a339195a11919ae52281e7a0d4d4515c
logta <takenobu15@gmail.com>
2020-02-24 02:15:05 +0900

    add vuetify and Work, Link Pages

---
+++ src/components/Header.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-bottom-navigation v-model="bottomNav">
+    <v-btn value="Home" href="/">
+      <span>Home</span>
+      <v-icon>mdi-history</v-icon>
+    </v-btn>
+
+    <v-btn value="About" href="/about">
+      <span>About</span>
+      <v-icon>mdi-heart</v-icon>
+    </v-btn>
+
+    <v-btn value="Work" href="/work">
+      <span>Work</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+
+    <v-btn value="Link" href="/link">
+      <span>Link</span>
+      <v-icon>mdi-map-marker</v-icon>
+    </v-btn>
+  </v-bottom-navigation>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      bottomNav: "recent"
+    };
+  }
+};
+</script>
